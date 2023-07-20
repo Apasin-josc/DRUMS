a67192f9b1c7e8afa1de0451c105e2fbf7d1aac3
José Omar <82555012+Apasin-josc@users.noreply.github.com>
2023-07-20 14:04:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//   alert(`i got clicked`);
+// }
+
+// document.querySelectorAll("button").addEventListener(`click`, function () {
+//   alert(`🐔`);
+// });
+
+const buttons = document.querySelectorAll(".drum").length;
+
+for (const i = 0; buttons; i++) {
+  addEventListener(`click`, function () {
+    alert(`🐔`);
+  });
+}
