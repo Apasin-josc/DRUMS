70e07496f8d92706f84a03f720939aff8e11e594
José Omar <82555012+Apasin-josc@users.noreply.github.com>
2023-07-24 12:33:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-// document.querySelector("button").addEventListener("click", handleClick);
+//detecting button press
+let buttons = document.querySelectorAll(".drum").length;
 
-// function handleClick() {
-//   alert(`i got clicked`);
-// }
+for (let i = 0; i < buttons; i++) {
+  document.querySelectorAll(`.drum`)[i].addEventListener(`click`, function () {
+    let buttonInnerHTML = this.innerHTML;
 
-// document.querySelectorAll("button").addEventListener(`click`, function () {
-//   alert(`🐔`);
-// });
+    makeSound(buttonInnerHTML);
 
-const buttons = document.querySelectorAll(".drum").length;
-
-for (const i = 0; buttons; i++) {
-  addEventListener(`click`, function () {
-    alert(`🐔`);
+    buttonAnimation(buttonInnerHTML);
   });
+}
+//detecting keyboard press down
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      const kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    default:
+      console.log(`esa letra no tiene sonido`);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
